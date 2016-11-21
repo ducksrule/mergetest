7858bde20bfc54257fe453be09b98501227bd6fd
Tom H <tharden@ramapo.edu>
2016-11-21 01:47:33 -0500

    mlml T1

--- Merging/Merging/Source.cpp
+++ Merging/Merging/Source.cpp
@@ -5,10 +5,6 @@ using namespace std;
 void main() {
 
 	cout << "Hello world" << endl;
-<<<<<<< HEAD
-	cout << "what are you doing" << endl;
-=======
-	cout << "What are you doing" << endl;
->>>>>>> 5a5329756c26f3b7b379e6ffae32cc2ef03f8aa0
+	cout << "Hello Moon" << endl;
 
 }
