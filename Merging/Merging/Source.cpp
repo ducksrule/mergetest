d7a4a5505504ddfe67a0e43a0a4c441986d83a56
test <tharden@ramapo.edu>
2016-11-21 01:51:50 -0500

    MT

--- Merging/Merging/Source.cpp
+++ Merging/Merging/Source.cpp
@@ -5,13 +5,6 @@ using namespace std;
 void main() {
 
 	cout << "Hello world" << endl;
-<<<<<<< HEAD
 	cout << "Hello Moon" << endl;
-=======
-
-	cout << "What are you doing" << endl;
-
-	cout << "Quack" << endl;
->>>>>>> bb4b1de86d1c17e05bdcf998f43b864063e15e19
 
 }
